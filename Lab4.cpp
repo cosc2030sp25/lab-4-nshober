e2e5f238ba732ed6eb26a20671c72c22b48b3727
nshober <nshober2@uwyo.edu>
2025-02-18 17:14:50 -0700

    :3

--- Lab4.cpp
+++ Lab4.cpp
@@ -18,90 +18,110 @@ bool parenCheck(string toCheck);
 /*DO NOT CHANGE MAIN OR THE FUNCTION DECLARATIONS*/
 int main()
 {
-  //paren strings
-  string paren1 = "(()((((()()()((()(((()()()()(((()(()()()(())()())()()))))()()()))()))()())())())))";
-  string paren2 = "()((((((()(((((()((()()()()()(()))))))))()))()(())(())(((((()()(())))(()()())(()))";
-  string paren3 = "(()((()(()()(()(((((()()(()()()((((()()(()()))()))))()()())))))()()())()()))())())";
-  string paren4 = "(()()(((()()(()(()()(()()()()()()(()(((((((((())())))))()))))()()))()())()()))()))";
-  string paren5 = "((())";
-  string paren6 = ")))(((";
+    //paren strings
+    string paren1 = "(()((((()()()((()(((()()()()(((()(()()()(())()())()()))))()()()))()))()())())())))";
+    string paren2 = "()((((((()(((((()((()()()()()(()))))))))()))()(())(())(((((()()(())))(()()())(()))";
+    string paren3 = "(()((()(()()(()(((((()()(()()()((((()()(()()))()))))()()())))))()()())()()))())())";
+    string paren4 = "(()()(((()()(()(()()(()()()()()()(()(((((((((())())))))()))))()()))()())()()))()))";
+    string paren5 = "((())";
+    string paren6 = ")))(((";
 
-  string rev1 = "sekopog";
-  string rev2 = "racecar";
-  string rev3 = "regnolsignirtssiht";
-  string rev4 = "wonybnwodsihtevahyllufepohdluohssyuguoy";
+    string rev1 = "sekopog";
+    string rev2 = "racecar";
+    string rev3 = "regnolsignirtssiht";
+    string rev4 = "wonybnwodsihtevahyllufepohdluohssyuguoy";
 
-  //Ignore this for now, these vectors are just part of the program
-  vector<string> parens;
-  parens.push_back(paren1);
-  parens.push_back(paren2);
-  parens.push_back(paren3);
-  parens.push_back(paren4);
-  parens.push_back(paren5);
-  parens.push_back(paren6);
+    //Ignore this for now, these vectors are just part of the program
+    vector<string> parens;
+    parens.push_back(paren1);
+    parens.push_back(paren2);
+    parens.push_back(paren3);
+    parens.push_back(paren4);
+    parens.push_back(paren5);
+    parens.push_back(paren6);
 
-  vector<string> rever;
-  rever.push_back(rev1);
-  rever.push_back(rev2);
-  rever.push_back(rev3);
-  rever.push_back(rev4);
-  
-  //call the parenCheck
-  for(int i=0; i<6; i++)
-  {
-    cout<<"Checking string"<<i+1<<endl;
-    if(parenCheck(parens.at(i)))
+    vector<string> rever;
+    rever.push_back(rev1);
+    rever.push_back(rev2);
+    rever.push_back(rev3);
+    rever.push_back(rev4);
+
+    //call the parenCheck
+    for (int i = 0; i < 6; i++)
     {
-      cout<<"Your code says that string"<<i+1<<" is good.";
-      if(i == 0 || i == 2 || i==3)
-      {
-        cout<<" Correct!"<<endl<<endl;
-      }
-      else
-      {
-        cout<<" Incorrect!"<<endl<<endl;
-      }
+        cout << "Checking string" << i + 1 << endl;
+        if (parenCheck(parens.at(i)))
+        {
+            cout << "Your code says that string" << i + 1 << " is good.";
+            if (i == 0 || i == 2 || i == 3)
+            {
+                cout << " Correct!" << endl << endl;
+            }
+            else
+            {
+                cout << " Incorrect!" << endl << endl;
+            }
+        }
+        else
+        {
+            cout << "Your code says that string" << i + 1 << " is bad.";
+            if (i == 1 || i == 4 || i == 5)
+            {
+                cout << " Correct!" << endl << endl;
+            }
+            else
+            {
+                cout << " Incorrect!" << endl << endl;
+            }
+        }
     }
-    else
-    {
-      cout<<"Your code says that string"<<i+1<<" is bad.";
-      if(i == 1 || i == 4 || i==5)
-      {
-        cout<<" Correct!"<<endl<<endl;
-      }
-      else
-      {
-        cout<<" Incorrect!"<<endl<<endl;
-      }
-    }
-  }
 
-  //now to reverse some strings
-  for(int i =0; i<4; i++)
-  {
-    cout<<"Reversing string"<<i+1<<endl;
-    cout<<stringReverse(rever.at(i))<<endl<<endl;
-  }
+    //now to reverse some strings
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Reversing string" << i + 1 << endl;
+        cout << stringReverse(rever.at(i)) << endl << endl;
+    }
 }
 
 //This function takes a string and reverses it using a stack
 //it returns the reversed string
 string stringReverse(string toReverse)
 {
-  stack<char> reverse; //declare the stack
-  string newString = "";
-  /*
-    Your code goes here
-  */
-  return newString;
+    stack<char> reverse; //declare the stack
+    string newString = "";
+    for (char letter : toReverse)
+    {
+        reverse.push(letter);
+    }
+    while (!reverse.empty()) {
+        newString += reverse.top();
+        reverse.pop();
+    }
+    return newString;
 }
 
 //this function uses a queue to check if a string of parens is properly matched
 bool parenCheck(string toCheck)
 {
-  queue<char> checker; //declare my queue
-  /*
-    Your code goes here
-  */
-  return false; //need a return statement to compile
+    queue<char> checker; //declare my queue
+    for (int i = 0; i < toCheck.size(); ++i)
+    {
+        if (toCheck[i] == ')') {
+            if (checker.empty()) {
+                return false;
+            }
+            else {
+                checker.pop();
+            }
+        }
+        else {
+            checker.push('(');
+        }
+    }
+    if (checker.empty())
+    {
+        return true;
+    }
+    else { return false; }
 }
